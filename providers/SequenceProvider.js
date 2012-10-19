282ca5f786fb514c267351e0dd94a2586a3d064c
ebensing <ebensing@rm-dash-r.com>
2012-10-19 11:55:57 -0400

    removed bcrypt require

--- providers/SequenceProvider.js
+++ providers/SequenceProvider.js
@@ -3,7 +3,6 @@ var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
-var bcrypt = require('bcrypt');
 var mongo = require("mongodb")
 var Sequence = (function () {
     function Sequence(title, imageIds, isActive) {
--- providers/SequenceProvider.ts
+++ providers/SequenceProvider.ts
@@ -5,7 +5,6 @@ var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
-var bcrypt = require('bcrypt');
 
 declare import mongo = module("mongodb");
 
