ad1efb7013a9ceb7801ea94544a7a899fcb993ce
ebensing <ebensing@rm-dash-r.com>
2012-10-19 15:56:13 -0400

    wrap it in document.ready

--- public/js/selectBox.js
+++ public/js/selectBox.js
@@ -1,46 +1,48 @@
-$("#mainImage").load(function () {
-    var $image = $('#mainImage');
-    var $container = $("<div>").addClass('image-overlay');
-    var $selection = $('<div>').addClass('selection-box');
-    $container.css({
-        'top': $image.position().top,
-        'left': $image.position().left,
-        'height': $image.height(),
-        'width': $image.width()
-    });
-    $("body").append($container);
-    $container.on('mousedown', function (e) {
-        var click_y = e.pageY;
-        var click_x = e.pageX;
-        $selection.css({
-            'top': click_y,
-            'left': click_x,
-            'width': 0,
-            'height': 0,
-            'background-color': 'red',
-            'z-index': 1000
+$("document").ready(function () {
+    $("#mainImage").load(function () {
+        var $image = $('#mainImage');
+        var $container = $("<div>").addClass('image-overlay');
+        var $selection = $('<div>').addClass('selection-box');
+        $container.css({
+            'top': $image.position().top,
+            'left': $image.position().left,
+            'height': $image.height(),
+            'width': $image.width()
         });
-        $selection.appendTo($container);
-        $container.on('mousemove', function (e) {
-            var move_x = e.pageX;
-            var move_y = e.pageY;
-            var width = Math.abs(move_x - click_x);
-            var height = Math.abs(move_y - click_y);
-
+        $("body").append($container);
+        $container.on('mousedown', function (e) {
+            var click_y = e.pageY;
+            var click_x = e.pageX;
             $selection.css({
-                'width': width,
-                'height': height
+                'top': click_y,
+                'left': click_x,
+                'width': 0,
+                'height': 0,
+                'background-color': 'red',
+                'z-index': 1000
             });
-            if(move_x < click_x) {
+            $selection.appendTo($container);
+            $container.on('mousemove', function (e) {
+                var move_x = e.pageX;
+                var move_y = e.pageY;
+                var width = Math.abs(move_x - click_x);
+                var height = Math.abs(move_y - click_y);
+
                 $selection.css({
-                    'left': click_x - width
+                    'width': width,
+                    'height': height
                 });
-            }
-            if(move_y < click_y) {
-                $selection.css({
-                    'top': click_y - height
-                });
-            }
+                if(move_x < click_x) {
+                    $selection.css({
+                        'left': click_x - width
+                    });
+                }
+                if(move_y < click_y) {
+                    $selection.css({
+                        'top': click_y - height
+                    });
+                }
+            });
         });
     });
 });
--- public/js/selectBox.ts
+++ public/js/selectBox.ts
@@ -1,52 +1,53 @@
 declare var $;
+$("document").ready(function () {
+    $("#mainImage").load(function () {
+        var $image = $('#mainImage');
+        var $container = $("<div>").addClass('image-overlay');
+        var $selection = $('<div>').addClass('selection-box');
 
-$("#mainImage").load(function() {
-    var $image = $('#mainImage');
-    var $container = $("<div>").addClass('image-overlay');
-    var $selection = $('<div>').addClass('selection-box');
-
-    $container.css({
-        'top': $image.position().top,
-        'left': $image.position().left,
-        'height': $image.height(),
-        'width' : $image.width()
-    });
-    $("body").append($container);
-
-    $container.on('mousedown', function(e) {
-        var click_y = e.pageY;
-        var click_x = e.pageX;
-
-        $selection.css({
-          'top':    click_y,
-          'left':   click_x,
-          'width':  0,
-          'height': 0,
-          'background-color' : 'red',
-          'z-index' : 1000
+        $container.css({
+            'top': $image.position().top,
+            'left': $image.position().left,
+            'height': $image.height(),
+            'width': $image.width()
         });
-        $selection.appendTo($container);
+        $("body").append($container);
 
-        $container.on('mousemove', function(e) {
-            var move_x = e.pageX,
-                move_y = e.pageY,
-                width  = Math.abs(move_x - click_x),
-                height = Math.abs(move_y - click_y);
+        $container.on('mousedown', function (e) {
+            var click_y = e.pageY;
+            var click_x = e.pageX;
 
             $selection.css({
-                'width':  width,
-                'height': height
+                'top': click_y,
+                'left': click_x,
+                'width': 0,
+                'height': 0,
+                'background-color': 'red',
+                'z-index': 1000
             });
-            if (move_x < click_x) { //mouse moving left instead of right
+            $selection.appendTo($container);
+
+            $container.on('mousemove', function (e) {
+                var move_x = e.pageX,
+                    move_y = e.pageY,
+                    width = Math.abs(move_x - click_x),
+                    height = Math.abs(move_y - click_y);
+
                 $selection.css({
-                    'left': click_x - width
+                    'width': width,
+                    'height': height
                 });
-            }
-            if (move_y < click_y) { //mouse moving up instead of down
-                $selection.css({
-                    'top': click_y - height
-                });
-            }
+                if (move_x < click_x) { //mouse moving left instead of right
+                    $selection.css({
+                        'left': click_x - width
+                    });
+                }
+                if (move_y < click_y) { //mouse moving up instead of down
+                    $selection.css({
+                        'top': click_y - height
+                    });
+                }
+            });
         });
     });
 });
