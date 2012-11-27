6f81dde35bf3445ee154719c7b44d473b0249d2d
E.J. Bensing <ebensing@rm-dash-r.com>
2012-11-27 11:29:43 -0500

    added bootstrap file

---
+++ setup/bootstrap.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+
+#setup script for ubuntu
+
+#get build-essential
+
+sudo apt-get update 
+sudo apt-get install build-essential
+
+#install git
+sudo apt-get install git-core
+
+#install node
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs npm
+
+#install mongo
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
