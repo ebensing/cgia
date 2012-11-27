cd915a9deafd0ab3e2ac2eda52472c7505db240c
E.J. Bensing <ebensing@rm-dash-r.com>
2012-11-27 11:05:50 -0500

    added db setup file

---
+++ setup/db_setup.js
@@ -0,0 +1,18 @@
+﻿
+// create the images
+
+db.images.insert({ title: "Map 1", url: "/images/final-map-1.png", votes: 0 });
+db.images.insert({ title: "Map 2", url: "/images/final-map-2.png", votes: 0 });
+db.images.insert({ title: "Map 3", url: "/images/final-map-3.png", votes: 0 });
+
+// get the ids
+
+var ids = db.images.find({}, { _id: true }).toArray()
+var idArr = [];
+for (var i = 0; i < ids.length; i++) {
+    idArr.push(ids[i]._id);
+}
+
+// create sequence
+
+db.sequences.insert({ title : "Job Title Maps", "imageIds" : idArr, "isActive" : true });
