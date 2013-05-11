71b009e10d646c7714ff1e9792818b8bc67e998d
EJ Bensing <ebensing@rm-dash-r.com>
2013-05-11 16:06:48 -0400

    fixed indexes

--- setup/create_map.js
+++ setup/create_map.js
@@ -8,7 +8,7 @@ var out_template = "'<%title%>' : <%pos%>,\n";
 
 for (var i = 0; i < seq.imageIds.length; i++) {
     var img = db.images.findOne({ _id: seq.imageIds[i] });
-    output += out_template.replace("<%title%>", img.title).replace("<%pos%>", i.toString());
+    output += out_template.replace("<%title%>", img.title).replace("<%pos%>", (i+1).toString());
 }
 
 output += "}";
