7004ecd17a45db382eaa115983fee0f7bbe1543e
EJ Bensing <ebensing@rm-dash-r.com>
2013-05-11 15:56:32 -0400

    added create_map.js

---
+++ setup/create_map.js
@@ -0,0 +1,16 @@
+﻿
+
+var seq = db.sequences.findOne();
+
+var output = "{";
+
+var out_template = "'<%title%>' : <%pos%>,\n";
+
+for (var i = 0; i < seq.imageIds.length; i++) {
+    var img = db.images.findOne({ _id: seq.imageIds[i] });
+    output += out_template.replace("<%title%>", img.title).replace("<%pos%>", i.toString());
+}
+
+output += "}";
+
+print(output);
