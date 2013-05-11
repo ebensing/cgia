3f0763b626142ef995731ac63b3aadc7a3a16557
EJ Bensing <ebensing@rm-dash-r.com>
2013-05-11 15:29:38 -0400

    changed line endings and stuff

---
+++ setup/create_map_and_import.ts
@@ -0,0 +1,25 @@
+/// <reference path='../nodelib/node.d.ts' />
+import fs = module("fs");
+
+var imageFolder = "../Public/images/huit_images/";
+var imgPath = "/images/huit_images/";
+
+var files = fs.readdirSync(imageFolder);
+
+var import_output = "";
+
+var import_template = 'db.images.insert({ title: "<%title%>", url: "<%url%>", votes: 0 });\n';
+
+for (var i = 0; i < files.length; i++) {
+    var title = files[i].replace(".png", "");
+    import_output += import_template.replace("<%title%>", title).replace("<%url%>", imgPath + files[i]);
+}
+
+import_output += 'var ids = db.images.find({}, { _id: true }).toArray() \
+    var idArr = [];\
+    for (var i = 0; i < ids.length; i++) { \
+        idArr.push(ids[i]._id); \
+    } \
+    db.sequences.insert({ title: "Job Title Maps", "imageIds": idArr, "isActive": true });';
+
+fs.writeFileSync('db_import_script.js', import_output);
